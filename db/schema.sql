553811f67acfab374850a93ff680bb03b4736236
Jadan Morrow <jadanmorrow@outlookcom>
2020-05-05 17:23:05 -0600

    updating files

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,11 @@
-/*
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-To run this file, we do the following in our Terminal:
+CREATE TABLE burgers (
 
-1. Go to the directory of this sql file.
-
-2. Get into our mysql console.
-
-3. Run "source schema.sql"
-
-*/
-
--- Drops the day_planner_db if it already exists --
-DROP DATABASE IF EXISTS day_planner_db;
-
--- Create the database day_planner_db and specified it for use.
-CREATE DATABASE day_planner_db;
-
-USE day_planner_db;
-
--- Create the table plans.
-CREATE TABLE plans (
   id int NOT NULL AUTO_INCREMENT,
-  plan varchar(255) NOT NULL,
+  `burger_name` varchar(255) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
-
--- Insert a set of records.
-INSERT INTO plans (plan) VALUES ('Plan to fight a ninja.');
